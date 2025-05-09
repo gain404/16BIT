c9a4a663827ee3c55b657ea1edaf85740a288e0f
KUJ <thsusekdnlt1@naver.com>
2025-05-09 10:06:34 +0900

    2025.05.09 (1)
    - ObjectHandler
    Muddle 타입에 따른 플레이어 상호작용 여부 판별
    
    = LevelManager
    게임 시작/종료 여부
    게임 진행 시간 및 시간 관련 별 획득 여부
    별 획득 여부
    플레이어가 획득한 보석 갯수
    총 획득 별 갯수
    싱글톤화 위한 Inatance 선언
    
    - Switch 스크립트 생성

--- Assets/Scripts/Objects/ObstacleHandler.cs
+++ Assets/Scripts/Objects/ObstacleHandler.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private ObstacleType obstacleType;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        PlayerController player = collision.GetComponent<PlayerController>(); //PlayerController가 Instance로 싱글톤화시 수정 필요
+        if (player != null)
+        {
+           // if (IsDangerForPlayer(player.playerType)) //PlayerController에서 public PlayerType playerType형식으로 지정 필요
+            {
+                Destroy(player.gameObject); // 위험하면 파괴
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool IsDangerForPlayer(PlayerType playerType)
     {
-        
+        switch (obstacleType)
+        {
+            case ObstacleType.FirePuddle:
+           //     return playerType != PlayerType.Fire; //같은 타입이 아니면 플레이어 Destroy처리
+            case ObstacleType.WaterPuddle:
+           //     return playerType != PlayerType.Water;
+            case ObstacleType.PoisonPuddle:
+                return true; //독 타일은 얄짤없이 플레이어 Destroy처리
+            default:
+                Debug.LogError("뭐야 이거 어떻게 닿았어");
+                return false;
+        }
     }
 }
+
+enum ObstacleType
+{
+    FirePuddle,
+    WaterPuddle,
+    PoisonPuddle
+}
+
