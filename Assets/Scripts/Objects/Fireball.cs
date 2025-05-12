151a0674dcd3f5033566872ca259eb92c68ae10a
Ddomyam <0101justin@naver.com>
2025-05-12 12:51:22 +0900

    feat : fireball rotation
    아래로 내려갈 때 방향전환되게 함

--- Assets/Scripts/Objects/Fireball.cs
+++ Assets/Scripts/Objects/Fireball.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class Fireball : MonoBehaviour
 {
+    Rigidbody2D _rigidbody;
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
         //일정 이하로 내려가면 삭제
@@ -13,6 +18,15 @@ public class Fireball : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+        //아래로 내려갈 때 방향전환
+        if (_rigidbody.velocity.sqrMagnitude > 0.01f)
+        {
+            float angle = Mathf.Atan2(_rigidbody.velocity.y, _rigidbody.velocity.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 8f * Time.deltaTime);
+        }
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
