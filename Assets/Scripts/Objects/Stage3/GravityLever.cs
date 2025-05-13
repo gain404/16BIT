c6c134960bd770a9af93df120abcc1d5bcc033da
Won0001 <smjwnaya1102@gmail.com>
2025-05-13 20:46:26 +0900

    feat : 방해 물체 추가, 중력 레버 애니메이션 추가
    - 방해 물체 추가, 중력 레버 애니메이션 추가

--- Assets/Scripts/Objects/Stage3/GravityLever.cs
+++ Assets/Scripts/Objects/Stage3/GravityLever.cs
@@ -8,8 +8,11 @@ public class GravityLever : MonoBehaviour
 
     private bool isPush = false;
 
+    private SpriteRenderer sr;
+
     private void Awake()
     {
+        sr = GetComponent<SpriteRenderer>();
         players = GameObject.FindGameObjectsWithTag("Player");
     }
 
@@ -39,8 +42,9 @@ public class GravityLever : MonoBehaviour
     private void PushGravityLever()
     {
        // Debug.Log("isPush : " + isPush);
-        if (Input.GetKeyDown(KeyCode.E) && isPush)
+        if (Input.GetKeyDown(KeyCode.Q) && isPush)
         {
+            sr.flipX = !sr.flipX;
             Debug.Log("isPush2 : " + isPush);
             if (GravityHandler.instance.gravityReversed == false) GravityHandler.instance.gravityReversed = true;
             else GravityHandler.instance.gravityReversed = false;
