d3a688a7b9f43684ee8c01d2c263674bcd1f5fd4
KUJ <thsusekdnlt1@naver.com>
2025-05-13 19:39:52 +0900

    fix : 씬에 따른 최고 별갯수 기록
    씬에 따른 최고 별갯수 기록

--- Assets/Scripts/Manager/LevelManager.cs
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -26,16 +27,25 @@ public class LevelManager : MonoBehaviour
 
     // 총 획득 별 갯수
     private int TotalStarAmount = 0;
+    private int HighTotalStarAmount; // 최고 별 갯수
+
+    // 레벨별 클리어 여부 변수
+    public Stage stage;
+    internal bool isLevel1Clear = false; // 레벨 클리어 여부
+    internal bool isLevel2Clear = false; // 레벨 클리어 여부
+    internal bool isLevel3Clear = false; // 레벨 클리어 여부
 
     // 싱글톤화 위한 Inatance 선언
     public static LevelManager Instance;
 
     private void Awake()
     {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
         if (Instance == null)
         {
             Instance = this;
-          //  DontDestroyOnLoad(gameObject);
+            //  DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -46,6 +56,7 @@ public class LevelManager : MonoBehaviour
     private void Start()
     {
         onGameStarted = true;
+        OnGameFinished();
     }
 
     private void Update()
@@ -69,30 +80,58 @@ public class LevelManager : MonoBehaviour
 
     private void OnGameFinished()
     {
+        Stage currentStage = GetCurrentStage();
+
+        switch (currentStage)
+        {
+            case Stage.Stage1:
+                isLevel1Clear = true;
+                Debug.Log("스테이지 1 클리어!");
+                break;
+
+            case Stage.Stage2:
+                isLevel2Clear = true;
+                Debug.Log("스테이지 2 클리어!");
+                break;
+
+            case Stage.Stage3:
+                isLevel3Clear = true;
+                Debug.Log("스테이지 3 클리어!");
+                break;
+        }
+
         onGameFinished = true;
         onGameStarted = false;
-        // 게임 종료 시 별 획득 여부에 따라 총 별 갯수 계산
-        if (getClearStar)
-        {
-            TotalStarAmount++;
-        }
-        if (getTimeStar)
-        {
-            TotalStarAmount++;
-        }
-        if (getZemStar)
-        {
-            TotalStarAmount++;
-        }
-        // 게임 종료 후 별 갯수 출력
+
+        if (getClearStar) TotalStarAmount++;
+        if (getTimeStar) TotalStarAmount++;
+        if (getZemStar) TotalStarAmount++;
+
         Debug.Log("총 획득 별 갯수: " + TotalStarAmount);
-        GetTotalStarAmount();
+
+        GetTotalStarAmount(currentStage); // 스테이지별 최고 점수 갱신
     }
 
-    private int GetTotalStarAmount()
+    private int GetTotalStarAmount(Stage stage)
     {
+        string key = $"HighTotalStar_{stage}";
+        int savedHigh = PlayerPrefs.GetInt(key, 0);
+
+        if (savedHigh < TotalStarAmount)
+        {
+            SetHighTotalStar(stage);
+        }
+
+
         return TotalStarAmount;
     }
+    private void SetHighTotalStar(Stage stage)
+    {
+        string key = $"HighTotalStar_{stage}";
+        PlayerPrefs.SetInt(key, TotalStarAmount);
+        PlayerPrefs.Save();
+    }
+
 
     private void IsGetZem()
     {
@@ -100,4 +139,23 @@ public class LevelManager : MonoBehaviour
         if (getZemAmount >= zemAmountMax) getZemStar = true;
     }
 
+    public enum Stage
+    {
+        Stage1,
+        Stage2,
+        Stage3
+    }
+
+    private Stage GetCurrentStage()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        return sceneName switch
+        {
+            "Stage1" => Stage.Stage1,
+            "Stage2" => Stage.Stage2,
+            "Stage3" => Stage.Stage3,
+            _ => throw new System.Exception("알 수 없는 씬 이름: " + sceneName)
+        };
+    }
 }
