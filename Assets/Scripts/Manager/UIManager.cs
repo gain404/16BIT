7fe31955d580b4a9e0f9bbf98cbe8cd3b11dcf3e
KUJ <thsusekdnlt1@naver.com>
2025-05-13 23:36:54 +0900

    Add : 게임 오버시 재시작 잘되게 변경 및 배경음악 추가, UImanager DontDestroy 삭제
    게임 오버시 재시작 잘되게 변경 및 배경음악 추가, UImanager DontDestroy 삭제

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -23,7 +23,7 @@ public class UIManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
+           // DontDestroyOnLoad(gameObject);
         }
 
         else
