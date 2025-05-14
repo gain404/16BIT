e75bd26d8ab7f6c229e41f70c7d5dbc41e8600a3
jangda0 <jang95dy@gmail.com>
2025-05-14 14:07:29 +0900

    docs: 메인씬 버튼 교체

--- Assets/Scripts/Manager/ButtonHandler.cs
+++ Assets/Scripts/Manager/ButtonHandler.cs
@@ -5,16 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour
 {
-    public void OnClickStartButton(string stageName) // 스테이지가 여러 개이기 때문에, 선택한 스테이지 이름과 같은 값을 입력
+    public void OnClickSceneChangeButton(string stageName) // 스테이지가 여러 개이기 때문에, 선택한 스테이지 이름과 같은 값을 입력
     {
         // 선택한 스테이지씬 이동
         // SceneManager.LoadScean("씬이름");
+        //GARALoadSceneManager.Instance.LoadScene(SceneType.StageSelectScene);
+
+    }
+
+    public void OnClickInGameButton() // 메인씬에서 stageSelectScene으로 넘어가는 버튼
+    {
+        SceneManager.LoadScene("StageSelectScene");
     }
 
     public void OnClickExitButton()
     {
         // 메인씬 이동
         // SceneManager.LoadScean("씬이름");
+    }
+
+    public void OnClickGameQuitButton()
+    {
+        // 게임종료
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+       Application.Quit();  
+#endif
     }
 
     public void OnClickRetryButton()
