0aa5074b3c52d3077447d77f128d8b5821b971d4
jangda0 <jang95dy@gmail.com>
2025-05-14 14:08:04 +0900

    docs: 스크립트 추후 삭제 예정.

--- Assets/Scripts/UI/IntoGameBtn.cs
+++ Assets/Scripts/UI/IntoGameBtn.cs
@@ -6,17 +6,26 @@ using UnityEngine.UI;
 
 public class IntoGameBtn : MonoBehaviour
 {
-    [SerializeField] private Button SceneChangeButton;
+    //    [SerializeField] private Button SceneChangeButton;
 
-    public void OnClickSceneChange()
-    {
-        if (GARALoadSceneManager.Instance == null)
-        {
-            Debug.LogError("LoadSceneManager.Instance가 null입니다.");
-            return;
-        }
+    //    public void OnClickSceneChange()
+    //    {
+    //        if (GARALoadSceneManager.Instance == null)
+    //        {
+    //            Debug.LogError("LoadSceneManager.Instance가 null입니다.");
+    //            return;
+    //        }
 
-        Debug.Log("StageSelectScene으로 전환을 시도합니다.");
-        GARALoadSceneManager.Instance.LoadScene(SceneType.StageSelectScene);
-    }
+    //        Debug.Log("StageSelectScene으로 전환을 시도합니다.");
+    //        GARALoadSceneManager.Instance.LoadScene(SceneType.StageSelectScene);
+    //    }
+
+    //    public void OnClickExitBtn()
+    //    {
+    //#if UNITY_EDITOR
+    //        UnityEditor.EditorApplication.isPlaying = false;
+    //#else
+    //       Application.Quit();  
+    //#endif
+    //    }
 }
--- Assets/Scripts/UI/StartButton.cs
+++ Assets/Scripts/UI/StartButton.cs
@@ -5,28 +5,29 @@ using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour
 {
-    [SerializeField] SceneType scene;
-    [SerializeField] Button sceneBtn;
-    [SerializeField] GameObject lockObject;
+    //[SerializeField] SceneType scene;
+    //[SerializeField] Button sceneBtn;
+    //[SerializeField] GameObject lockObject;
 
-    [SerializeField] bool isOpenStage;
-    void Start()
-    {
-        sceneBtn.onClick.RemoveAllListeners();
-        sceneBtn.onClick.AddListener(() => OnClickStartBtn());
+    //[SerializeField] bool isOpenStage;
+    //void Start()
+    //{
+    //    sceneBtn.onClick.RemoveAllListeners();
+    //    sceneBtn.onClick.AddListener(() => OnClickStartBtn());
 
-    }
-    public void OnClickStartBtn()
-    {
-        if (isOpenStage)
-        {
-            GARALoadSceneManager.Instance.LoadScene(scene);
-            //LevelManager.Instance.OnClickStart();
-        }
-    }
-    public void OpenStage()
-    {
-        isOpenStage = true;
-        lockObject?.SetActive(!isOpenStage);
-    }
+    //}
+    //public void OnClickStartBtn()
+    //{
+    //    if (isOpenStage)
+    //    {
+    //        GARALoadSceneManager.Instance.ChangeLevel(scene);
+    //        GARALoadSceneManager.Instance.LoadScene(scene);
+    //    }
+    //}
+    //public void OpenStage()
+    //{
+    //    isOpenStage = true;
+    //    lockObject?.SetActive(!isOpenStage);
+    //}
+
 }
